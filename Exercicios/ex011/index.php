eafbaeb8cd0b4e6f73364ede8ebdb955ed3d5a42
Vitor Satake <vitor.scastro2013@hotmail.com>
2023-10-12 17:26:16 -0300

    Novos exercicios

---
+++ Exercicios/ex011/index.php
@@ -0,0 +1,62 @@
+<?php
+// loops
+// WHILE
+// 1 para 10
+// $x = 0;
+
+// while($x<8){
+//     echo "$x<br>"; // para pular uma lina ou
+//     echo $x. "<br>"; // para pular uma lina
+//     $x++;
+// }
+
+// // 10 para 1
+// $x = 10;
+
+// while ($x >= 1) {
+//     echo "$x<br>"; // para pular uma lina
+//     $x--;
+// }
+
+// // DO WHILE
+// $i = 0;
+
+// do {
+//     echo "<br>" . $i . "<br>";
+
+//     $i++;
+// } while ($i <= 10);
+
+// FOR 
+
+
+// for($i = 20; $i >= 1; $i--){ // diminuindo de 1 em 1
+//     echo $i."<br>"; 
+// }
+// echo "<br>";
+// for($i = 1; $i <= 20; $i++){ // aumentando de 1 em 1
+//     echo $i."<br>"; 
+// }
+// echo "<br>";
+// for($i = 1; $i <= 20; $i++){  // aumentando de 1 em 1, se os numeros forem pares
+//         if($i%2==0){
+//             echo $i."<br>"; 
+//         }
+    
+// }
+// $colors = array("red", "yellow", "blue");
+
+// foreach($colors as $item){ // pega um por um dos itens
+//     echo $item."<br>";
+// }
+
+// ARRAY
+
+$numeros = array(1,2,3,4,5,6);
+echo $numeros[5]; // pegando o valor na posição 4, sendo que começa em 0
+
+
+ // shift + alt + f = tabular bloco
+ // ctrl + ; = comentar bloco
+
+ ?>
